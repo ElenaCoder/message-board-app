299c994f7727d819825935b05b66d593c6119bfb
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2024-01-23 23:07:10 +0200

    feat: make index.js file is inter level point

---
+++ server/src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const channelsRouter = require('./routes/channels');
+const messagesRouter = require('./routes/messages');
+
+// Enable CORS for all routes
+app.use(cors());
+
+
+app.use('/api/channels', channelsRouter);
+app.use('/api/messages', messagesRouter);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
