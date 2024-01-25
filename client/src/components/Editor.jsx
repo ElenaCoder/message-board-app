6021cdc98a608f2b589c8b5a773d93be06f4ed04
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2024-01-25 15:25:55 +0200

    feat: add simple optimistic update handle-Editor comp.

--- client/src/components/Editor.jsx
+++ client/src/components/Editor.jsx
@@ -4,35 +4,44 @@ import PropTypes from 'prop-types';
 import './Editor.css';
 
 const Editor = ({ selectedChannel, onAddMessage }) => {
-    const [message, setMessage] = useState('');
+    const [newMessage, setNewMessage] = useState('');
 
     const handleSubmit = async () => {
-        if (!message || !selectedChannel) {
-            // Handle invalid input or channel
-            return;
-        }
+        if (!newMessage.trim() || !selectedChannel) return;
+
+        // Optimistically update the UI with the new message
+        onAddMessage(selectedChannel, newMessage.trim());
+        setNewMessage(''); // Clear the input field
 
         try {
-            // Send the new message to the server
             await axios.post(
                 `http://localhost:5000/messages/${selectedChannel}`,
                 {
-                    text: message,
+                    text: newMessage.trim(),
                 },
             );
 
-            onAddMessage(selectedChannel, message);
-
-            // Clear the input field after submission
-            setMessage('');
+            // If the server response is successful, do nothing as the optimistic update is already there
         } catch (error) {
-            console.error('Error adding message:', error);
+            // If there's an error, retrieve all messages for the channel and update the UI
+            try {
+                const response = await axios.get(
+                    `http://localhost:5000/messages/${selectedChannel}`,
+                );
+                const newMessages = response.data;
+                onAddMessage(selectedChannel, newMessages);
+            } catch (error) {
+                console.error(
+                    'Error retrieving messages after failed POST:',
+                    error,
+                );
+            }
         }
     };
 
     // Clear input when selectedChannel changes
     useEffect(() => {
-        setMessage('');
+        setNewMessage('');
     }, [selectedChannel]);
 
     return (
@@ -40,10 +49,10 @@ const Editor = ({ selectedChannel, onAddMessage }) => {
             <div className='editor'>
                 <textarea
                     placeholder='Type a message...'
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
+                    value={newMessage}
+                    onChange={(e) => setNewMessage(e.target.value)}
                 ></textarea>
-                <button disabled={!message} onClick={handleSubmit}>
+                <button disabled={!newMessage.trim()} onClick={handleSubmit}>
                     Send
                 </button>
             </div>
